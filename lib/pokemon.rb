faa5f50534d78ad8065d927031ad0fbdf6d34937
Brittany Morris <brittmmendez@gmail.com>
2018-01-12 12:53:56 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,11 +1,11 @@
 class Pokemon
 
-attr_accessor :name, :type, :db
+attr_accessor :name, :type
 
-def initialize(name, type, db)
+def self.initialize(name, type, @db)
   @name=name
   @type=type
-  @db=db
+  
 end
 
 
