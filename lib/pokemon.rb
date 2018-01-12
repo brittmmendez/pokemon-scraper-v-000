fbedf86a0c8e242e19baf27f4fc2119108b06a23
Brittany Morris <brittmmendez@gmail.com>
2018-01-12 13:04:29 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -9,12 +9,12 @@ def initialize(id:nil, name:, type:, db:)
   @db=db
 end
 
-def self.save
+def self.save(name,type,db)
   sql=<<-SQL
   INSERT INTO pokemon (name, type) VALUES (?,?)
   SQL
 
-  DB[:conn].execute(sql,self.name,self,type)
+  db.execute(sql,self.name,self,type)
   end
 
 end
