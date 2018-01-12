1758a9ad8706ad874db02f7343d5d61eb1109c1a
Brittany Morris <brittmmendez@gmail.com>
2018-01-12 13:02:31 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -7,9 +7,16 @@ def initialize(id:nil, name:, type:, db:)
   @name=name
   @type=type
   @db=db
-
-
 end
 
+def save
+  sql=<<-SQL
+  INSERT INTO pokemon (name, type) VALUES (?,?)
+  SQL
+
+  DB[:conn].execute(sql,self.name,self,type)
+  @id=DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+  
+end
 
 end
